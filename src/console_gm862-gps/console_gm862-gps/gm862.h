b2077b7ce4862d67e0c34f89739b77c7aebc2ec1
Sébastien VERRY <sebastienverry@gmail.com>
2016-04-23 16:24:16 +0200

    com1

--- src/console_gm862-gps/console_gm862-gps/gm862.h
+++ src/console_gm862-gps/console_gm862-gps/gm862.h
@@ -1,1 +1,32 @@
+/* gm862.hpp
+*/
+
+#ifndef C__RS232_HPP_INCLUDED
+#define C__RS232_HPP_INCLUDED
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+class c__rs232
+{
+public:
+	c__rs232(void);
+	c__rs232(LPCWSTR port);
+	~c__rs232(void);
+
+	BOOL confCom(int baud_rate = CBR_9600, BOOL f_parity = TRUE, int byte_size = 8, int parity = ODDPARITY, int stop_bit = TWOSTOPBITS);
+	BOOL TxData(char *buffer, unsigned long number_bytes_buffer);
+	BOOL TxData(std::string &buffer, unsigned long number_bytes_buffer);
+	// BOOL TxData(char*buffer, char [11],unsigned long number_bytes_buffer);
+	unsigned int RxData(char *buffer, unsigned long number_bytes_buffer);
+
+
+private:
+	BOOL setTimeout(int TO);
+	HANDLE hCom;
+
+};
+
+#endif // C__RS232_HPP_INCLUDED
 
---
+++ src/console_gm862-gps/console_gm862-gps/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <conio.h>
+#include <fstream>
+#include <Windows.h>
+#include "gm862.h"
+using namespace std;
+
+#ifdef _UNICODE
+typedef wchar_t TCHAR;
+#else
+typedef char TCHAR;
+#endif // _UNICODE
+
+typedef const TCHAR* LPCTSTR;
+
+int main(void)
+{
+	char RxBuf[256];
+	int NbRecus;
+	char phone_number[] = "phone number";
+	char dest[256];
+	string msg;
+	char caract;
+	//  sprintf(dest,"AT+CMGS=\"%s\"\\r",phone_number);
+	//sprintf(dest,"AT+CPIN=\""",phone_number,strlen("AT+CMGS\r"));
+	LPCWSTR comport = L"\\\\.\\COM1";
+
+	c__rs232 my_rs(comport);
+
+typedef const TCHAR* LPCTSTR;
+	my_rs.confCom(CBR_9600, FALSE, 8, NOPARITY, ONESTOPBIT);
+
+	strcpy_s(dest, 5,"AT\r");
+	my_rs.TxData(dest, strlen(dest));
+	NbRecus = my_rs.RxData(RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+
+	sprintf_s(dest, 12,"AT+CMGF=1\r");  //  Commande AT pour avoir le message en mode texte.
+	my_rs.TxData(dest, strlen(dest));
+	NbRecus = my_rs.RxData(RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+
+
+
+	sprintf_s(dest, 10+sizeof(phone_number),"AT+CMGS=\"%s\"\r", phone_number); //  Commande AT pour envoyer un message en SMS.
+	my_rs.TxData(dest, strlen(dest));
+	NbRecus = my_rs.RxData((char *)RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+	do // ecrire un messsage tant que l'onappuie  pas sur Ctrl+Z.
+	{
+		caract = _getch();
+		msg += caract;
+		cout << msg << endl;
+
+	} while (caract != 0x1A);
+	my_rs.TxData((char *)msg.c_str(), msg.size()); //  envoi un message en SMS.
+
+	NbRecus = my_rs.RxData((char *)RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+	NbRecus = my_rs.RxData((char *)RxBuf, sizeof RxBuf); // Reception du message.
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;  // Reception du message.
+
+
+	my_rs.TxData((char *)"AT+CMGL\r", strlen("AT+CMGL\r")); //  Commande AT pour la reception d'un message.
+
+
+	NbRecus = my_rs.RxData((char *)RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+	NbRecus = my_rs.RxData((char *)RxBuf, sizeof RxBuf);
+	RxBuf[NbRecus] = '\0';
+	cout << "NbRecus : " << NbRecus << " Data: " << RxBuf << endl;
+
+	ofstream fichier("test.txt", ios::out | ios::trunc); // Introduire le message reçu dans un fichier .txt.
+	if (fichier)
+	{
+		fichier << RxBuf << endl;
+		fichier.close();
+	}
+	else {
+		cerr << "Imopossible d'ouvrir le fichier !!" << endl;
+	}
+	system("pause");
+
+
+
+}
